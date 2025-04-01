262b19fc250f985505d8aa6397fca29e25a44c33
shadrack-hosea <hoseashadrack007@gmail.com>
2025-04-01 13:52:03 -0700

    betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
 /*
  * Entry point
  */
-int main(void) {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);	
+int main(void) 
+{
+    	puts("\"Programming is like building a multilingual puzzle");
+    	return(0);	
 }
