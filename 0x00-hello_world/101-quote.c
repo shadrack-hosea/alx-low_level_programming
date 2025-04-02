07638f86a346b2c313c46f54c9f1400815cc9b80
shadrack-hosea <hoseashadrack007@gmail.com>
2025-04-02 12:49:23 -0700

    101-qoute.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    	write(2, msg, 59);
+    	return (1);
+}
