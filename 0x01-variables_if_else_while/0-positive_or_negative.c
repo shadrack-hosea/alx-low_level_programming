a10da964b82d96590ce33a47f1fdfe1b3f398c3c
shadrack-hosea <hoseashadrack007@gmail.com>
2025-04-03 04:33:48 -0700

    improved betty coding style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,10 +9,8 @@
 int main(void)
 {	
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n == 0) 
 	{
 		printf("%i is zero\n", n);
@@ -25,5 +23,5 @@ int main(void)
 	{
 		printf("%i is negative\n", n);	 
 	}
-	return 0;
+	return (0);
 }
