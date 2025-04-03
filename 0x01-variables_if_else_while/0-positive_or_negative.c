ff356baa6941b4fc9ba09c7a7561628f853f177b
shadrack-hosea <hoseashadrack007@gmail.com>
2025-04-03 04:27:34 -0700

    fix print typo

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n == 0)
+	if (n == 0) 
+	{
 		printf("%i is zero\n", n);
+	}
 	if (n > 0)
 	{
 		printf("%i is positive\n", n);
 	} else {
-		printf("%i is negative]n", n);	 
+		printf("%i is negative\n", n);	 
 	}
-
-
 	return(0);
 }
