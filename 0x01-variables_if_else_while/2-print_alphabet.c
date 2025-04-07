cdc4a32571165a913aefd113bc4e81b9c36960ce
shadrack-hosea <hoseashadrack007@gmail.com>
2025-04-07 05:02:27 -0700

    added git lower aphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return - Always returns 0 (Success)
+ */
+int main(void)
+{
+	int lowerStartIndex = 97;
+	int lowerEndIndex = 122;
+
+	while (lowerStartIndex <= lowerEndIndex)
+	{
+		putchar(lowerStartIndex);
+		lowerStartIndex++;
+	}
+	putchar('\n');
+}
