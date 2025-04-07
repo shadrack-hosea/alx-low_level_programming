785303188f18d5e918c0d596d48b33ad845f64de
shadrack-hosea <hoseashadrack007@gmail.com>
2025-04-07 07:23:35 -0700

    print hexadeciaml

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always return 0(Success)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 15; i++)
+	{
+		if (i < 10)
+		{
+			putchar(i + '0');
+		} else 
+		{
+			putchar(i - 10 + 'a');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
