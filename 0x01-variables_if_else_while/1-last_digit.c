4243f1fb31e388a07e2869473a2f128fb1bad461
shadrack-hosea <hoseashadrack007@gmail.com>
2025-04-07 03:55:05 -0700

    fix: zero last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	lastDigit = n % 10;
 	if (lastDigit == 0)
 	{
-		printf("Last digit of %i is %i and is greater than 5\n", n, lastDigit);
+		printf("Last digit of %i is %i and is 0\n", n, lastDigit);
 	}
 	else if (lastDigit > 5)
 	{
