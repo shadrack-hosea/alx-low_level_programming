60dca4eb1da455947abb8addacd58114e5b8d30f
shadrack-hosea <hoseashadrack007@gmail.com>
2025-04-07 06:47:44 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ * Return: Always return 0(Success)
+ *
+ */
+int main(void)
+{
+	int initial = 0;
+
+	while (initial < 10)
+	{
+		printf("%i", initial);
+		initial++;
+	}
+	printf("\n");
+	return (0);
+}
