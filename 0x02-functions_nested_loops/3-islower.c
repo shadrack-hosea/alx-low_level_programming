59e0f307cb5053b1ea248a36b819f2423ad37584
shadrack-hosea <hoseashadrack007@gmail.com>
2025-04-11 06:30:08 -0700

    defined islower to check if cahracter is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.j"
+
+/*
+ * _isLower - check for lowercase character
+ *
+ * @c: character integer
+ *
+ * Return: 1 if c is lower case 0 otherwise
+ *
+ */
+int _isLower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else 
+		return (0);
+}
+
