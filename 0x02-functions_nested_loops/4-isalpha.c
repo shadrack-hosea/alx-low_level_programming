41d89da9a20a8d633108584ddcc052af6946bb23
shadrack-hosea <hoseashadrack007@gmail.com>
2025-04-11 07:17:31 -0700

    improved isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <ctype.h>
 
 /*
  * _isalpha - checks for alphabetic character
@@ -11,7 +10,7 @@
  */
 int _isalpha(int c)
 {
-	if(isalpha(c))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z' ))
 		return 1;
 	else
 		return 0;
