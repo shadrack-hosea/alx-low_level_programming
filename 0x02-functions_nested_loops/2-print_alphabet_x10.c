73802697a425a6b3e784ee7180f9e7f6ca270133
shadrack-hosea <hoseashadrack007@gmail.com>
2025-04-11 06:16:47 -0700

    improved by using nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,14 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char letter;
 
 	for (i = 0; i < 10; i++)
 	{
-		print_alphabet();
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
 	}
+	_putchar('\n');
 }
