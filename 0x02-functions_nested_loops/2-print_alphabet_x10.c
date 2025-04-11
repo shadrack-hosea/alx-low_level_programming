97e45522c98ff509b5302819add48c4109d90dc4
shadrack-hosea <hoseashadrack007@gmail.com>
2025-04-11 06:11:57 -0700

    print alphabet in lower case 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+ *
+ * print_alphabet_x10 - print alphabet in lower case 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+}
