c98038bf7e6f48ebee51949e05914854805ba352
shadrack-hosea <hoseashadrack007@gmail.com>
2025-04-11 03:04:01 -0700

    documented _putchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,14 @@
-#include <stdio.h>
+#include <unistd.h>
 #include "main.h"
 
 /*
- * _putchar - print _putchar
+ * _putchar - print character
+ * @c: character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately
  */
-void _putchar(void)
+int  _putchar(char c)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	return write(1, &c, 1);
 }
